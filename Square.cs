a9e597ea0de73c709c6d583545d54ca5e7ab2968
Jarek <jaroslaw.pela@wp.pl>
2023-11-14 23:14:05 +0100

    added Interface

---
+++ Square.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_Interface_Excercise
+{
+    public class Square
+    {
+    }
+}
