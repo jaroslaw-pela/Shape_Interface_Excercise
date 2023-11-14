cc259bfb1174d80aa6675a30aa26e6956faeb06e
Jarek <jaroslaw.pela@wp.pl>
2023-11-14 23:00:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Shape_Interface_Excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
